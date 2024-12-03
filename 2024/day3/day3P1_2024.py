b9a9d10cee37d1749d70319836a50ba8ba3354b5
Arnaud-VH <arnaud.vanhees@icloud.com>
2024-12-03 13:51:59 +0100

    Day 3 Part 1: Solved (Regex)

---
+++ 2024/day3/day3P1_2024.py
@@ -0,0 +1,18 @@
+import re
+from functools import reduce
+
+def readInput():
+   with open("in.txt", "r") as file:
+      lines = file.readlines()
+      return ''.join(lines)
+
+def solveMul():
+   allInput = readInput()
+   val = 0
+   allTuples = re.findall(r'mul\(\d+\,\d+\)', allInput)
+   for tp in allTuples:
+      found = re.findall(r'\d+', tp)
+      val += (int(found[0]) * int(found[1]))
+   return val
+      
+print(solveMul())
