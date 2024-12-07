b16f9b37b3973aea86d7cbf1b263502116a1838c
Arnaud-VH <arnaud.vanhees@icloud.com>
2024-12-07 15:21:10 +0100

    Day 7 Part 1 and 2: Solved (Recursive Solution)

---
+++ 2024/day7/day7P1_2024.py
@@ -0,0 +1,31 @@
+def readInput():
+   file = open("in.txt")
+   operators = []
+   targets = []
+   for line in file:
+      split = line.strip().split(":")
+      numbers = [int(num) for num in split[1].split()]
+      operators.append(numbers)
+      targets.append(int(split[0]))
+   return (targets, operators)
+
+def checkOperators(target, numbers, val):
+   if (target == val and not numbers):
+      return True
+   elif numbers:
+      return (checkOperators(target, numbers[1:], val+numbers[0]) or checkOperators(target, numbers[1:], val*numbers[0]))
+   return False
+
+
+
+def solve():
+   targets, numbers = readInput()
+   val = 0
+   for i in range(len(targets)):
+      if (checkOperators(targets[i], numbers[i], 0)):
+         val += targets[i]
+   return val
+
+print(solve())
+
+
